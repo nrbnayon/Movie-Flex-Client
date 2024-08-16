c839df31867e65fec3cdc31be2b316bcd0e82620
nrbnayon <nrbnayon@gmail.com>
2024-08-17 00:33:34 +0600

    added jwt token

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -61,15 +61,15 @@ const AuthProvider = ({ children }) => {
         photoURL: currentUser?.photoURL,
       };
       setUser(currentUser);
-      // if (currentUser) {
-      //   axiosPublic.post("/jwt", loggedUser).then((res) => {
-      //     if (res.data.token) {
-      //       localStorage.setItem("access-token", res.data.token);
-      //     }
-      //   });
-      // } else {
-      //   localStorage.removeItem("access-token");
-      // }
+      if (currentUser) {
+        axiosPublic.post("/jwt", loggedUser).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
       setLoading(false);
     });
     return () => {
